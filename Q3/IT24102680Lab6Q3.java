9ed5fb06119d8ab220f0d6ca116fea0dcced021c
IT24102680 <it24102680@my.sliit.lk>
2024-09-04 08:51:31 +0530

    IT24102680_Lab6_Q3

---
+++ Q3/IT24102680Lab6Q3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class IT24102680Lab6Q3{
+
+  public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+	int count = 0;
+	double sumOfSquares = 0.0;
+
+	while (true)
+ 	{
+	  System.out.print("Enter a number:");
+  	  int number = input.nextInt();
+  
+  	  if (number == -99)
+	  {
+    	    break; // Terminate input if -99 is entered
+    	  }
+
+  	  if(number<0)
+	  {
+    	    System.out.println("Invalid input. Please enter a positive integer or -99 to terminate.");
+	    continue; // Skip the current iteration and ask for input again
+ 	  }
+
+ 	  sumOfSquares += Math.pow(number,2);
+ 	  count++;
+	}
+
+	double rms = Math.sqrt(sumOfSquares/count);
+
+	System.out.print("\nThe Root Mean Square (RMS) is: " +rms);
+
+  }
+}
